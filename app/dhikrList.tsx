cc218313ee34bef64127ceb35dbf2070cfeaca25
Kevin <kemalsogut7c@gmail.com>
2024-08-01 15:29:59 -0400

    dhikr 0.1.3.1

--- app/dhikrList.tsx
+++ app/dhikrList.tsx
@@ -1,12 +1,56 @@
-import { View, Text } from 'react-native'
+import { View, Text, FlatList, StyleSheet } from 'react-native'
 import React from 'react'
 
+const DATA = [
+    {
+      id: '01',
+      title: 'SubhannAllah',
+      count:3,
+    },
+    {
+      id: '02',
+      title: 'Elhamdulillah',
+      count:33,
+    },
+    {
+      id: '03',
+      title: 'Allahuekber',
+      count:333,
+    },
+  ];
+  type ItemProps = {title: string, count:number};
+  const Item = ({title, count}:ItemProps) => (
+    <View style={styles.container}>
+      <Text style={styles.list}> {title}: {count}</Text>
+    </View>
+  );
 const dhikrList = () => {
   return (
-    <View>
-      <Text>dhikrList</Text>
-    </View>
+    
+      <FlatList 
+        data={DATA}
+        renderItem={({item}) => <Item  title={item.title} count={item.count}/>}
+        keyExtractor={item => item.id}
+        
+      />
+    
   )
 }
 
 export default dhikrList
+
+
+const styles = StyleSheet.create({
+    list:{
+        alignItems:'center',
+        justifyContent:'center',
+        fontSize:22,
+        
+       
+    },
+    container:{
+        flex:1,
+        alignItems:'center',
+        justifyContent:'center',
+    }
+  });

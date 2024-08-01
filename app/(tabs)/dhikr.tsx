d89a08b04720f0f8d50cc893e8aab2ad33260fcd
Kevin <kemalsogut7c@gmail.com>
2024-08-01 11:18:04 -0400

    dhikr .1.2

--- app/(tabs)/dhikr.tsx
+++ app/(tabs)/dhikr.tsx
@@ -1,5 +1,6 @@
 import Ionicons from '@expo/vector-icons/Ionicons';
-import { StyleSheet, Image, Platform, Text, View, TouchableOpacity, Alert} from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Image, Platform, Text, View, TouchableOpacity, Alert} from 'react-native';
 
 import { Collapsible } from '@/components/Collapsible';
 import { ExternalLink } from '@/components/ExternalLink';
@@ -10,15 +11,21 @@ import { ThemedView } from '@/components/ThemedView';
 
 
 export default function TabTwoScreen() {
-
+  const [count, setCount] = useState(0);
 
   const handlePress = () => {
-    Alert.alert('Image Pressed!', 'You pressed the image.');
+    setCount(count + 1);
+  };
+  const reset = () => {
+    setCount(0);
   };
 
-
+  
   return (
-    <View>
+    <View style={styles.container}>
+      <View style={styles.dash}>
+        <Text style={styles.dashText}>{count}</Text>
+      </View>
       <View style={styles.circle}>
         <TouchableOpacity onPress={handlePress}>
           <Image
@@ -26,6 +33,10 @@ export default function TabTwoScreen() {
           style={styles.image}
           />
         </TouchableOpacity>
+
+        <TouchableOpacity  onPress={reset}>
+        <Text>Reset</Text>
+      </TouchableOpacity>
       </View>
 
     </View>
@@ -34,9 +45,21 @@ export default function TabTwoScreen() {
 }
 
 const styles = StyleSheet.create({
-  circle:{
+  dashText:{
+    fontSize: 55,
+  },
+  dash:{
     flex:1,
     justifyContent:'center',
+    alignItems: 'center',
+  },
+  container:{
+    height:'100%',
+  },
+  circle:{
+    flex:1,
+    paddingBottom:100,
+    justifyContent:'flex-end',
     alignItems: 'center',
 
  },

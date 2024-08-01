9993f9bbdbee1d82c2ed4764df1b4f7ed0151a91
Kevin <kemalsogut7c@gmail.com>
2024-08-01 10:39:45 -0400

    dhikr 0.1.2

--- app/(tabs)/dhikr.tsx
+++ app/(tabs)/dhikr.tsx
@@ -18,23 +18,18 @@ export default function TabTwoScreen() {
 
 
   return (
-    <View style={styles.circle}>
+    <View>
+      <View style={styles.circle}>
       <TouchableOpacity onPress={handlePress}>
         <Image
           source={require('@/assets/images/Circle_(transparent).png')}
           style={styles.image}
         />
       </TouchableOpacity>
-    
-    
-      <div style={styles.border}>
-        <View style={styles.border}>
-          <Text>
-            Test
-          </Text>
-        </View>
-      </div>
     </View>
+
+    </View>
+    
   );
 }
 
@@ -51,33 +46,19 @@ const styles = StyleSheet.create({
   },
   circle: {
     flex: 1,
-    justifyContent: 'flex-end',
-    paddingBottom: 100,
-    alignItems: 'center',
+    
+    justifyContent: 'center',
+    
+    alignItems:"center"
   },
   image: {
     width: 200,
     height: 200,
     borderRadius: 10,
   },
-  plus: {
-    position: 'absolute',
-    bottom: 10,
-    right: 10,
-    fontSize: 24,
-    color: 'black', // Adjust the color as needed
-  },
-  minus: {
-    position: 'absolute',
-    bottom: 10,
-    left: 10,
-    fontSize: 24,
-    color: 'black', // Adjust the color as needed
-  },
   border: {
     width:'100%',
     borderWidth:1,
     borderColor:'black'
-
   }
 });
